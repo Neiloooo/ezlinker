5650dccd79663439604b97cd47897c22d45726c8
wwhai <751957846@qq.com>
2019-12-10 15:05:30 +0800

    :sparkles: update thread executor

--- app/src/main/java/com/ezlinker/app/scriptengine/luaengine/LuaEngine.java
+++ app/src/main/java/com/ezlinker/app/scriptengine/luaengine/LuaEngine.java
@@ -2,15 +2,9 @@ package com.ezlinker.app.scriptengine.luaengine;
 
 import com.ezlinker.app.scriptengine.EZScriptEngine;
 import lombok.extern.java.Log;
-import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.Resource;
-import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @author wangwenhai
@@ -22,22 +16,8 @@ import java.util.Map;
 public class LuaEngine extends EZScriptEngine {
 
     @Override
-    protected boolean start() {
-        return false;
-    }
-
-    @Override
     protected Object executeScript(String script) throws ScriptException {
         return null;
     }
 
-    @Override
-    protected boolean stop() {
-        return false;
-    }
-
-    @Override
-    protected Map<String, Object> info() {
-        return null;
-    }
 }
